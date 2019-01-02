294849029a0e09c8550309d9e3236a166ba729bb
picsou20 <xav.riquet@gmx.fr>
2019-01-02 11:32:26 +0100

    version 1

--- index.php
+++ index.php
@@ -4,12 +4,16 @@
 	<title>Git site test</title>
 </head>
 <body>
-<header>
-	<h1>Site Git test</h1>
-</header>
+	<header>
+		<h1>Site Git test</h1>
+	</header>
 
-<main>
-	corps du site git
-</main>
+	<main>
+		corps du site git
+	</main>
+
+	<footer>
+		pied de page du site git
+	</footer>
 </body>
 </html>
